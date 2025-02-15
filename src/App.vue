7cd31f9c7d848d9dd7a98033c4abdb6910391032
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-02-15 01:13:21 +0100

    feat: tabs navbar refactor

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/vue';
-import { homeOutline, settingsOutline, bookOutline } from 'ionicons/icons';
+<script lang="ts" setup>
+import {IonApp, IonRouterOutlet, IonTabs} from '@ionic/vue';
+import TabsNavbar from "@/views/TabsNavbar.vue";
 </script>
 
 <template>
   <ion-app>
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
-      <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="home" href="/home">
-          <ion-icon :icon="homeOutline" />
-          <ion-label>Home</ion-label>
-        </ion-tab-button>
-        <ion-tab-button tab="bible" href="/bible">
-          <ion-icon :icon="bookOutline" />
-          <ion-label>Bible</ion-label>
-        </ion-tab-button>
-        <ion-tab-button tab="settings" href="/settings">
-          <ion-icon :icon="settingsOutline" />
-          <ion-label>Settings</ion-label>
-        </ion-tab-button>
-      </ion-tab-bar>
+      <TabsNavbar/>
     </ion-tabs>
   </ion-app>
 </template>
@@ -40,16 +27,16 @@ import { homeOutline, settingsOutline, bookOutline } from 'ionicons/icons';
 /* Paper theme */
 [data-theme="paper"] {
   --ion-background-color: #f5e6d3;
-  --ion-background-color-rgb: 245,230,211;
+  --ion-background-color-rgb: 245, 230, 211;
   --ion-text-color: #2c1810;
-  --ion-text-color-rgb: 44,24,16;
+  --ion-text-color-rgb: 44, 24, 16;
   --ion-border-color: #d4b594;
 }
 
 [data-theme="dark"] {
   --ion-background-color: #121212;
-  --ion-background-color-rgb: 18,18,18;
+  --ion-background-color-rgb: 18, 18, 18;
   --ion-text-color: #ffffff;
-  --ion-text-color-rgb: 255,255,255;
+  --ion-text-color-rgb: 255, 255, 255;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path, {resolve} from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      romcal: resolve(__dirname, 'node_modules/romcal/dist/esm/romcal.mjs'),
+    },
+  },
+
+  build: {
+    sourcemap: false
+  }
 })
