0665b204f916b0d2c4e29dde9dd0eb1a738b3883
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-03-05 16:59:02 +0100

    feat: fix router pages, display in mobile

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,44 +1,50 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router'
-import { RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory} from '@ionic/vue-router'
+import {RouteRecordRaw} from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import SettingsPage from '../views/SettingsPage.vue'
 import BiblePage from '../views/BiblePage.vue'
 import SaintPage from "@/components/organism/SaintPage.vue";
+import TabsNavbar from "@/components/molecules/TabsNavbar.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  },
-  {
-    path: '/bible',
-    name: 'Bible',
-    component: BiblePage
-  },{
-    path: '/saint',
-    name: 'Saint',
-    component: SaintPage
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: SettingsPage
-  },
-  {
-    path: '/prayer/:type',
-    name: 'Prayer',
-    component: () => import('../components/organism/PrayerPage.vue')
-  }
+    {
+        path: '/',
+        component: TabsNavbar,
+        children: [
+            {
+                path: "",
+                redirect: "/home",
+            },
+            {
+                path: "home",
+                name: "home",
+                component: HomePage,
+            }, {
+                path: '/bible',
+                name: 'Bible',
+                component: BiblePage
+            },
+            {
+                path: '/settings',
+                name: 'Settings',
+                component: SettingsPage
+            },
+            {
+                path: '/prayer/:type',
+                name: 'Prayer',
+                component: () => import('../components/organism/PrayerPage.vue')
+            }, {
+                path: '/saint',
+                name: 'Saint',
+                component: SaintPage
+            },
+        ],
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router
