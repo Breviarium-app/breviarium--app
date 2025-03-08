5cded959add287196b800c1e49c22c28d79554c0
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-03-08 19:48:24 +0100

    feat: version number with collaboration GitHub

---
+++ src/components/molecules/OpenSourceGitHubComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="colab">
+    <a href="https://breviarium--site.pages.dev/" target="_blank">
+      <ion-icon :icon="logoGithub" class="icon-github"/>
+      <br/>
+      {{ $t('opensource_join_us') }}
+    </a>
+  </div>
+</template>
+<script lang="ts" setup>
+import {IonIcon} from "@ionic/vue";
+import {logoGithub} from 'ionicons/icons';
+</script>
+<style scoped>
+.icon-github {
+  width: 26px;
+  height: 26px;
+  vertical-align: -7px;
+}
+
+.colab {
+  margin-top: 1em;
+}
+</style>
--- src/components/molecules/VersionNumber.vue
+++ src/components/molecules/VersionNumber.vue
@@ -1,10 +1,19 @@
 <template>
   <ion-col>
-    <p class="version">{{ $t('title') }} · Version {{ versionApp }}</p>
+    <div class="version">
+      <p>
+        {{ $t('title') }} · Version {{ versionApp }}
+      </p>
+      <p>
+        <OpenSourceGitHubComponent/>
+      </p>
+    </div>
   </ion-col>
 </template>
 <script lang="ts" setup>
 import {version} from "../../../package.json"
+import {IonCol} from "@ionic/vue";
+import OpenSourceGitHubComponent from "@/components/molecules/OpenSourceGitHubComponent.vue";
 
 const versionApp = version
 </script>
