cd080b1abf48362faa13e14e047b02e4cfe97223
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-04-29 01:15:41 +0200

    feat: add IntermediumPage.vue tertia, sexta and nona pages

---
+++ src/components/organism/prayers/SextaPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <IntermediumPage :prayer-lib="brev.getSexta" prayer-title="Sexta"></IntermediumPage>
+</template>
+<script lang="ts" setup>
+
+import IntermediumPage from "@/components/organism/prayers/IntermediumPage.vue";
+import Breviarium from "breviarium";
+
+const brev = new Breviarium();
+
+</script>
+<style lang="css" scoped>
+</style>
