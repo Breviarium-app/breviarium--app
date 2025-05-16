955677652c52261cfdc5b3ee52797a0f48b60215
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-05-16 03:06:59 +0200

    feat: saint page layout for website use PrayerPage

--- src/components/organism/SaintPage.vue
+++ src/components/organism/SaintPage.vue
@@ -1,51 +1,41 @@
 <script lang="ts" setup>
-import {IonBackButton, IonButtons, IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar} from '@ionic/vue';
+import {IonLabel} from '@ionic/vue';
 import {useSanctusStore} from "@/stores/sanctus.ts";
 import {MONTH_SPANISH} from "@/constants";
 import {ref} from "vue";
+import PrayerPage from "@/components/organism/PrayerPage.vue";
 
 const {selectedSaint, saintsOfDay} = useSanctusStore();
 let todaySaint = ref<any | undefined>(selectedSaint);
 </script>
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-back-button default-href="/" text="Atrás"></ion-back-button>
-        </ion-buttons>
-        <ion-title>Santo del día</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content class="ion-padding">
-      <h2>{{ todaySaint?.name }}</h2>
-      <p>
+  <PrayerPage>
+    <h2>{{ todaySaint?.name }}</h2>
+    <p>
         <span v-if="todaySaint?.day">• Festividad: {{ todaySaint?.day }} de {{
             MONTH_SPANISH[todaySaint?.month - 1]
           }}</span>
-        <span v-if="todaySaint?.birth"><br/>• Nacimiento: {{ todaySaint?.birth }}</span>
-        <span v-if="todaySaint?.dead"><br/>• Muerte: {{ todaySaint?.dead }}</span>
-        <span v-if="todaySaint?.meaning"><br/>• Significado: {{ todaySaint?.meaning }}</span>
-        <span v-if="todaySaint?.description"><br/>📜 {{ todaySaint?.description }}</span>
+      <span v-if="todaySaint?.birth"><br/>• Nacimiento: {{ todaySaint?.birth }}</span>
+      <span v-if="todaySaint?.dead"><br/>• Muerte: {{ todaySaint?.dead }}</span>
+      <span v-if="todaySaint?.meaning"><br/>• Significado: {{ todaySaint?.meaning }}</span>
+      <span v-if="todaySaint?.description"><br/>📜 {{ todaySaint?.description }}</span>
+    </p>
+
+    <div v-if="saintsOfDay.length > 1">
+      <h4>Más santos</h4>
+      <p>En el día de hoy la Iglesia celebra también:</p>
+      <ul>
+        <li v-for="(saint, index) in saintsOfDay.slice(1)" :key="index">{{ saint.name }}</li>
+      </ul>
+    </div>
+
+    <ion-label>
+      <p class="footer">
+        <a href="https://breviarium.es/" target="_blank">Breviarium Project</a> • <a
+          href="https://breviarium.es/sanctus/" target="_blank">Sanctus</a>
       </p>
-
-      <div v-if="saintsOfDay.length > 1">
-        <h4>Más santos:</h4>
-        <p>En el día de hoy la Iglesia celebra también a:</p>
-        <ul>
-          <li v-for="(saint, index) in saintsOfDay.slice(1)" :key="index">{{ saint.name }}</li>
-        </ul>
-      </div>
-
-      <ion-label>
-        <p class="footer">
-          <a href="https://breviarium.es/" target="_blank">Breviarium Project</a> • <a
-            href="https://breviarium.es/sanctus/" target="_blank">Sanctus</a>
-        </p>
-      </ion-label>
-
-    </ion-content>
-  </ion-page>
+    </ion-label>
+  </PrayerPage>
 </template>
 <style scoped>
 p {
