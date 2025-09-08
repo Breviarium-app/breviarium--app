fba79eb9bcf1253dafb6a734939367a78cecedb5
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-09-08 03:00:20 +0200

    feat: add types, and display alternatives from EvangeliumAndLectiones.vue #55

--- src/components/organism/prayers/EvangeliumAndLectiones.vue
+++ src/components/organism/prayers/EvangeliumAndLectiones.vue
@@ -2,31 +2,39 @@
   <PrayerPage title="Evangelio y lecturas">
     <h3 class="center">{{ $t('cycle') }} {{ liturgyInfo?.cycle }}</h3>
     <h3 class="center">{{ liturgyInfo?.celebration }}, {{ rank }}</h3>
-    <div v-for="(prayer) in prayers">
-      <!--      <h3 v-if="prayer?.celebraciones_posibles[index]" class="center"-->
-      <!--          v-html="formatText(prayer?.celebraciones_posibles[index])"></h3>-->
-      <div v-for="(lecture) in prayer?.lecturas" class="lecture-block">
-        <h4 class="title-color" v-html="formatText(lecture.ref)"></h4>
-        <p v-html="formatTextLecture(lecture.texto)"></p>
-        <p v-if="lecture.type == LectureTypes.GOSPEL || lecture.type == LectureTypes.CELEBRATION_GOSPEL" class="right">
-          {{ t('theGospelOfTheLord') }}</p>
-        <p v-if="lecture.type == LectureTypes.FIRST_LECTURE || lecture.type == LectureTypes.SECOND_LECTURE || lecture.type == LectureTypes.CELEBRATION_FIRST_LECTURE || lecture.type == LectureTypes.CELEBRATION_SECOND_LECTURE"
-           class="right">{{ t('theWordOfTheLord') }}</p>
+    <div v-for="item in lecturesBlocks">
+      <ion-segment v-if="item.lectures.value.length > 1" v-model="item.selected.value">
+        <ion-segment-button v-for="(_item, index) in item.lectures.value" :value="(index)">
+          <ion-label>Opción {{ index + 1 }}</ion-label>
+        </ion-segment-button>
+      </ion-segment>
+      <div v-for="(lecture, index) in item.lectures.value" class="lecture-block">
+        <div v-if="item.selected.value == index">
+          <h4 class="title-color" v-html="formatText(lecture.ref)"></h4>
+          <p v-html="formatTextLecture(lecture.texto)"></p>
+          <p v-if="item.responseKey" class="right">{{ t(item.responseKey) }}</p>
+        </div>
       </div>
     </div>
   </PrayerPage>
 </template>
 <script lang="ts" setup>
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import PrayerPage from "@/components/organism/PrayerPage.vue";
-import {LecturesSchemaOutput} from "breviarium/dist/prayer-manager-interface";
+import {LecturesSchemaOutput, SingleLectureSchemaOutput} from "breviarium/dist/prayer-manager-interface";
 import {formatText, formatTextLecture} from "@/constants/formatText.ts";
 import {LectureTypes} from "@/constants/types.ts";
 import {useI18n} from 'vue-i18n'
 import {useBreviariumStore} from "@/stores/breviarium.ts";
 import {rankTranslate} from "@/constants/utils.ts";
+import {IonLabel, IonSegment, IonSegmentButton} from "@ionic/vue";
 
 const {t} = useI18n()
+
+const selectedFirst = ref<number>(0);
+const selectedPsalm = ref<number>(0);
+const selectedSecond = ref<number>(0);
+const selectedGospel = ref<number>(0);
 
 onMounted(async () => {
 
@@ -50,7 +58,7 @@ onMounted(async () => {
     if (prayers.value?.length === 0) {
       prayers.value = data;
     }
-    // console.log("prayers.value", prayers.value)
+    console.log("prayers.value", prayers.value)
   });
 
 });
@@ -59,6 +67,44 @@ const prayers = ref<LecturesSchemaOutput[]>();
 const liturgyInfo = ref();
 const rank = ref();
 
+const firstLectures = ref<SingleLectureSchemaOutput[]>([]);
+const psalmLectures = ref<SingleLectureSchemaOutput[]>([]);
+const secondLectures = ref<SingleLectureSchemaOutput[]>([]);
+const gospelLectures = ref<SingleLectureSchemaOutput[]>([]);
+
+watch(prayers, () => {
+  console.log("prayers", prayers.value)
+
+  prayers.value?.forEach(prayer => {
+    prayer.lecturas.forEach(lecture => {
+      if ([LectureTypes.FIRST_LECTURE, LectureTypes.FIRST_LECTURE_ALTERNATIVE, LectureTypes.FIRST_LECTURE_CELEBRATION].includes(lecture.type as LectureTypes)) {
+        firstLectures.value?.push(lecture)
+      }
+      if ([LectureTypes.PSALM, LectureTypes.PSALM_CELEBRATION].includes(lecture.type as LectureTypes)) {
+        psalmLectures.value?.push(lecture)
+      }
+      if ([LectureTypes.SECOND_LECTURE, LectureTypes.SECOND_LECTURE_ALTERNATIVE, LectureTypes.SECOND_LECTURE_CELEBRATION].includes(lecture.type as LectureTypes)) {
+        secondLectures.value?.push(lecture)
+      }
+      if ([LectureTypes.GOSPEL, LectureTypes.GOSPEL_SHORT, LectureTypes.GOSPEL_CELEBRATION].includes(lecture.type as LectureTypes)) {
+        gospelLectures.value?.push(lecture)
+      }
+    })
+  });
+
+  console.log("firstLectures", firstLectures.value)
+  console.log("psalm", psalmLectures.value)
+  console.log("second", secondLectures.value)
+  console.log("gospel", gospelLectures.value)
+
+})
+
+const lecturesBlocks = [
+  {lectures: firstLectures, selected: selectedFirst, responseKey: 'theWordOfTheLord'},
+  {lectures: psalmLectures, selected: selectedPsalm, responseKey: ''},
+  {lectures: secondLectures, selected: selectedSecond, responseKey: 'theWordOfTheLord'},
+  {lectures: gospelLectures, selected: selectedGospel, responseKey: 'theGospelOfTheLord'},
+]
 </script>
 <style lang="css" scoped>
 .lecture-block {
