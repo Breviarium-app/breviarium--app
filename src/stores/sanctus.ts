9e03ef64538c7680aec56b43051937ebda1b9000
Miguel Martínez Serrano <mmartinez@nextdigital.es>
2025-09-13 12:53:55 +0200

    feat: update saints list after on dateChange

--- src/stores/sanctus.ts
+++ src/stores/sanctus.ts
@@ -5,11 +5,12 @@ import {Ref, ref} from "vue";
 
 export const useSanctusStore = defineStore("sanctus", () => {
     const sanctus = new Sanctus();
-    const saintsOfDay = sanctus.getSaintsOfDay();
+    const saintsOfDay: Ref<SaintInfo[] | undefined> = ref(sanctus.getSaintsOfDay());
     const selectedSaint: Ref<SaintInfo | undefined> = ref(sanctus.getSaint());
 
     const updateSaint = (date: Date) => {
         selectedSaint.value = sanctus.getSaint(date);
+        saintsOfDay.value = sanctus.getSaintsOfDay(date);
     }
     return {selectedSaint, sanctus, saintsOfDay, updateSaint};
 });
